b4415b8bb31a8488e37f1abb58fc365e0facce2e
AliBeiti <116284588+AliBeiti@users.noreply.github.com>
2025-06-26 15:00:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,16 @@ SENT_FILE = "sent_offers.txt"
 if not os.path.exists(SENT_FILE):
     open(SENT_FILE, 'w').close()
 
-with open(SENT_FILE, 'r',encoding="utf-8", errors="ignore") as f:
+with open(SENT_FILE, 'r') as f:
     sent_offers_keys = set(line.strip() for line in f)
-print("file read")
+
 # --- Telegram send  function ---
 
 
 def send_to_channel(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {"chat_id": CHANNEL_ID, "text": message}
+    payload = {"chat_id": CHANNEL_ID,
+               "text": message, "parse_mode": "Markdown"}
     response = requests.post(url, payload)
     print(response.status_code, response.text)
 
@@ -31,10 +32,12 @@ def send_to_channel(message):
 def process_offers(new_offers):
     global sent_offers_keys
     for offer in new_offers:
-        offer_key = f'{offer["town"]}|{offer["header"]}|{offer["price"]}|{offer["area"]}|{offer["available"]}'
+        offer_key = f"{offer["town"]}|{offer["header"]}|{offer["price"]}|{offer["area"]}|{offer["available"]}"
         if offer_key not in sent_offers_keys:
             if create_message(offer) != 0:
+                print("I am here")
                 send_to_channel(create_message(offer))
+                print(create_message(offer))
                 sent_offers_keys.add(offer_key)
                 with open(SENT_FILE, 'a') as f:
                     f.write(offer_key + "\n")
@@ -44,14 +47,15 @@ def check_for_offer():
     url = "https://www.stwdo.de/wohnen/aktuelle-wohnangebote#residential-offer-list"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    print(soup.select_one('option[value="Dortmund"]').get_text().strip())
+
     Dortmund = soup.select_one(
         'option[value="Dortmund"]').get_text().strip() == "Dortmund"
     if Dortmund:
-        offers = soup.select(".teaser-body")
+        offers = soup.select("div.teaser.js-link-area")
         # print(f"offers{offers}")
         offers_list = []
         for offer in offers:
+            link = offer["data-href"]
             town = offer.select_one(".subheader-5").get_text()
             header = offer.select_one(".headline-5").get_text()
             body = offer.select_one(
@@ -60,10 +64,11 @@ def check_for_offer():
             area = body[3].get_text()
             available = body[5].get_text()
             dic = {"town": town, "header": header, "price": price,
-                   "area": area, "available": available}
-            # print(dic)
+                   "area": area, "available": available, "link": "https://www.stwdo.de" + link}
+            print(dic)
             offers_list.append(dic)
         process_offers(offers_list)
+
     else:
         print("no offers")
 
@@ -74,7 +79,9 @@ def create_message(offer):
         message += f"{offer['header']}\n\n"
         message += f"Price: {offer['price']}\n"
         message += f"Area: {offer['area']}\n"
-        message += f"Available from: {offer['available']}\n"
+        message += f"Available from: {offer['available']}\n\n"
+        # message += f'<a href={offer['link']}>Apply Now.</a>'
+        message += f"[Apply Now]({offer['link']})"
         return message
     else:
         return 0
